a3a2c4f78bf9d8ef17d786a005f22937c12818ba
WenSui Liu <liuwensui@gmail.com>
2020-05-09 16:27:01 -0500

    Create qcut.py

---
+++ code/qcut.py
@@ -0,0 +1,7 @@
+def qcut(x, n):
+  _q = numpy.linspace(0, 100, n, endpoint = False)[1:]
+  _x = [_ for _ in x if not numpy.isnan(_)]
+  _c = numpy.unique(numpy.percentile(_x, _q, interpolation = "lower"))
+  return([_ for _ in _c])
+  
+  
