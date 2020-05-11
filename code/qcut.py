fc213e86544325cfb14c65f5c9825374c5b1b3f2
WenSui Liu <liuwensui@gmail.com>
2020-05-11 09:26:22 -0500

    Update qcut.py

--- code/qcut.py
+++ code/qcut.py
@@ -1,8 +1,24 @@
 def qcut(x, n):
+  """
+  The function to discretize a numeric vector into n pieces based on quantiles
+
+  Parameters:
+    x: A numeric vector.
+    n: An integer indicating the number of categories to discretize.
+
+  Returns:
+    A list of numeric values to divide the vector x into n categories.
+
+  Example:
+    qcut(range(10), 3)
+    # [3, 6]
+  """
+
   _q = numpy.linspace(0, 100, n, endpoint = False)[1:]
   _x = [_ for _ in x if not numpy.isnan(_)]
   _c = numpy.unique(numpy.percentile(_x, _q, interpolation = "lower"))
   return([_ for _ in _c])
+
   
 qcut(range(10), 3)
 # [3, 6]
